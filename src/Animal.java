2303c18707aa059ddc5423e0b65746bffa254710
Voiku Dmitriy <voiku173dmitry@gmail.com>
2023-05-26 04:57:12 +0300

    changes 2

---
+++ src/Animal.java
@@ -0,0 +1,6 @@
+package src;
+
+public class Animal {
+    public String name;
+    public int age;
+}
--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ package src;
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello world!");
+        System.out.println("my first program transported to GIT");
     }
 }
